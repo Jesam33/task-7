23912a2a4d4c118439563cfa21458f206fbeb48f
jesam33 <obasesamgospel@gmail.com>
2024-08-08 14:03:03 +0100

    updates

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,11 +4,18 @@ export async function connectToMongoDB() {
   try {
     const uri = process.env.DB_URL;
     if (!uri) {
-      throw new Error('MONGODB_URI is not defined');
+      throw new Error('DB_URL is not defined');
     }
-    await mongoose.connect(uri);
+    
+    // Connect to MongoDB with options
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Optionally include other options such as `useCreateIndex` or `useFindAndModify`
+    });
+
     console.log('Connected to Database');
   } catch (error) {
-    console.error('Error connecting to Database', error);
+    console.error('Error connecting to Database:', error);
   }
 }
