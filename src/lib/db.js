68a84a138ad7b81648cf88a431fd960c75096b4e
jesam33 <obasesamgospel@gmail.com>
2024-08-08 13:37:05 +0100

    updates

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectToMongoDB() {
   try {
-    const uri = process.env.DB_URL;
+    const uri = process.env.NEXT_PUBLIC_DB_URL;
     if (!uri) {
       throw new Error('MONGODB_URI is not defined');
     }
